9b59cc98a84d02df2d1b29cb13406ccab2f695c4
YS739 <suk921@gmail.com>
2022-12-30 11:10:48 +0900

    Fix error - todos.map is not an object add {} in Add todo useState

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ export default function App() {
   const [text, setText] = useState("");
 
   // Add todo
-  const [todos, setTodos] = useState();
+  const [todos, setTodos] = useState({});
 
   const newTodo = {
     id: Date.now(),
